63164bdb8da8f158db47f6d5e6baa5fd7de7381c
Dafydd James <dafydd@cantab.net>
2012-10-21 11:09:09 +0100

    Problem 5

---
+++ problem5.py
@@ -0,0 +1,27 @@
+# Problem 5
+
+# "2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+# What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?"
+
+# This took a long time to do 
+
+def dividesBy(n):
+    numbers = range(1, n+1)
+    numbers.reverse()
+    # print numbers
+    i = 0
+    looking = True
+    while(looking):
+        looking = False
+        i += n
+        print i
+        for x in numbers:
+            if(i % x != 0):
+                looking = True
+                break
+    return i
+
+start = time.time()
+print "Answer = %i " % dividesBy(19)
+end = time.time()
+print "Duration = %s" % end - start
