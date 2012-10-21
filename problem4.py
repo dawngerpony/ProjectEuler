8a97a6818cadf470b59fa2b8e726c5049927e3e3
Dafydd James <dafydd@cantab.net>
2012-10-21 10:15:58 +0100

    Problem 4
    Solution to problem 4

---
+++ problem4.py
@@ -0,0 +1,38 @@
+# Problem 4
+
+# A palindromic number reads the same both ways. 
+# The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 99.
+# Find the largest palindrome made from the product of two 3-digit numbers.
+
+def isPalindrome(n):
+    '''
+    Uses the funky Python [::-1] notation to determine whether a string
+    is equal to the reverse of the string, i.e. is a palindrome."
+    '''
+    if(str(n) == str(n)[::-1]):
+        return True
+    else:
+        return False
+
+def findLargestPalindrome(numbers):
+    '''
+    Finds the largest palindrome from a list of factors:
+    1. Create a set of tuples containing all combinations of the elements in the list.
+    2. Create a list containing the product of all the tuples.
+    3. Sort/reverse the list so the highest number comes first (an optimization)).
+    4. Return the highest number that matches the qualification of being a palindrome.
+    '''
+    tuples = []
+    for x in numbers:
+        tuples += [[x,y] for y in numbers]
+    products = [x*y for x,y in tuples]
+    products.sort()
+    products.reverse()
+    for x in products:
+        if(isPalindrome(x)):
+            return x
+        
+if __name__=='__main__':
+    numbers = range(100, 1000)
+    print findLargestPalindrome(numbers)
+    
